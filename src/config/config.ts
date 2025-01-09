cd50256b7ba913b25a400e98d513b0ac8b0685a3
NoobWolf1 <malayshukla21@gmail.com>
2025-01-10 02:45:02 +0530

    pre-containerisation steps

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 import { Dialect } from "sequelize";
+
 export const dbConfig = {
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10),
@@ -14,16 +15,4 @@ export const jwtConfig = {
   secret: process.env.SECRET,
   expiry: process.env.TOKEN_EXPIRY_HOUR,
   saltRound: 3,
-};
-
-export const emailConfig = {
-  emailService: process.env.EMAIL_SERVICE,
-  emailUser: process.env.EMAIL_USER,
-  emailPassword: process.env.EMAIL_PASS,
-  emailFrom: process.env.EMAIL_FROM,
-};
-
-export const otpConfig = {
-  otpExpiry: process.env.OTP_EXPIRY_MIN,
-  otpSecret: process.env.OTP_SECRET,
 };
--- src/routes/v1/authRoute.ts
+++ src/routes/v1/authRoute.ts
@@ -1,18 +1,15 @@
 import { Router } from "express";
 import { validateRequest } from "../../middleware";
 import {
-  forgotPassword,
   loginUser,
   registerUser,
-  resetPassword,
 } from "../../controllers/auth";
 import { loginSchema, registerSchema } from "../../validation/user";
+
 const authRouter = Router();
 
 authRouter.post("/register", validateRequest(registerSchema), registerUser);
 authRouter.post("/login", validateRequest(loginSchema), loginUser);
-authRouter.post("/forgot-password", forgotPassword);
-authRouter.post("/reset-password", resetPassword);
 
 export default authRouter;
 
@@ -96,70 +93,4 @@ export default authRouter;
  *       "401":
  *         description: Invalid email or password
  *
- */
-
-/**
- * @swagger
- * /v1/auth/forgot-password:
- *   post:
- *     summary: Forgot password
- *     description: An email will be sent to reset password.
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *             properties:
- *               email:
- *                 type: string
- *                 format: email
- *             example:
- *               email: fake@example.com
- *     responses:
- *       "204":
- *         description: No content
- *       "404":
- *         description: Email not found
- *
- */
-
-/**
- * @swagger
- * /v1/auth/reset-password:
- *   post:
- *     summary: Reset password
- *     tags: [Auth]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             required:
- *               - email
- *               - password
- *               - otp
- *             properties:
- *               email:
- *                 type: string
- *                 format: email
- *               password:
- *                 type: string
- *                 format: password
- *               otp:
- *                 type: string
- *             example:
- *               email: name@example.com
- *               password: password1
- *               otp: 123344
- *     responses:
- *       "204":
- *         description: No content
- *       "401":
- *         description: Password reset failed
-
  */
